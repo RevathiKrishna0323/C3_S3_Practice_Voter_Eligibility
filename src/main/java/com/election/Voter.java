b0b294fe49242722a2bd96820b4db1863dc1dffa
Revathi G <revathikrishna0323@gmail.com>
2022-08-25 16:41:22 +0530

    Declared the Attributes to the Voter class.

--- src/main/java/com/election/Voter.java
+++ src/main/java/com/election/Voter.java
@@ -1,6 +1,12 @@
+package com.election;
+
 /*
  * Author Name: Revathi
  * Date: 25-08-2022
  * Created With: IntelliJ IDEA Community Edition
  */
-
+public class Voter {
+    private String name;
+    private int age;
+    static final int VOTER_ELIGIBLE_AGE = 18;
+    private Object voter;
